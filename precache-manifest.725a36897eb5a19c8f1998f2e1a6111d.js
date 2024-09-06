f36da3a12a8056c8beb4c78df0e695e6b9813a46
Chris Hammersley <chris-hammersley@users.noreply.github.com>
2024-09-06 10:50:21 -0700

    Updates

---
+++ precache-manifest.725a36897eb5a19c8f1998f2e1a6111d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65a164eff685b8d2531d16a836c463c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c11c175d4c6a6871ef8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "455a1bc1ef574805b201",
+    "url": "/static/js/2.075c04fc.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.075c04fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c11c175d4c6a6871ef8",
+    "url": "/static/js/main.1ffbc18c.chunk.js"
+  },
+  {
+    "revision": "e457b04d1d31d140c96f",
+    "url": "/static/js/runtime-main.ffd4c120.js"
+  }
+]);
